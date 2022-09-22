1dacfc363ae793e83ba283345616f8b6618d8cbf
PeterEikore <PeterEikore@VAT-L-DEV2>
2022-09-22 17:23:39 +0100

    Add project files.

---
+++ EMSApp/Models/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMSApp.Models
+{
+    public class Result
+    {
+        public Employee Employee { get; set; }
+        public Department Department { get; set; }
+    }
+}
